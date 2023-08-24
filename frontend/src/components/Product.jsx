94e15a29c3b8bfe6b5a4e01b4efdcfcc7d673d70
Ruimeng <u7504184@anu.edu.au>
2023-08-25 00:58:39 +1000

    Finish Fetch Products

--- frontend/src/components/Product.jsx
+++ frontend/src/components/Product.jsx
@@ -1,20 +1,25 @@
 import React from 'react'
 import { Card } from 'react-bootstrap'
+import {Link} from'react-router-dom'
+import Rating from './Rating'
 
 const Product = ({product}) => {
   return (
     <Card className='my-3 p-3 rounded'>
-        <a href={`/product/${product._id}`}>
+        <Link to={`/product/${product._id}`}>
             <Card.Img src={product.image} variant='top'/>
-        </a>
+        </Link>
 
         <Card.Body>
-            <a href={`/product/${product._id}`}>
-                <Card.Title as="div">
+            <Link to={`/product/${product._id}`}>
+                <Card.Title as="div" className='product-title'>
                     <strong>{product.name}</strong>
-
+{/* 这个是让即使产品名称只有一行，也不会出现卡片缩小一截的问题 */}
                 </Card.Title>
-            </a>
+            </Link>
+            <Card.Text as='div'>
+                <Rating value={product.rating} text={`${product.numReviews} reviews`}/>
+            </Card.Text>
             <Card.Text as='h3'>
                 ${product.price}
             </Card.Text>
--- frontend/src/screens/HomeScreen.jsx
+++ frontend/src/screens/HomeScreen.jsx
@@ -1,17 +1,33 @@
-import React from 'react'
-import {Row, Col} from 'react-bootstrap'
-import products from '../products'
+import React, { useEffect, useState } from 'react'
+import { Row, Col } from 'react-bootstrap'
+import axios from 'axios'
 import Product from '../components/Product'
+import originalProducts from '../products'  // Renamed
 
 const HomeScreen = () => {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    const fetchProducts = async () => {
+      try {
+        const { data } = await axios.get('http://localhost:5001/api/products');
+        setProducts(data);
+      } catch (error) {
+        console.error("There was an error fetching the data", error);
+        setProducts(originalProducts);  // Use originalProducts if API call fails
+      }
+    };
+    fetchProducts();
+  }, []);
+
   return (
     <>
       <h1>Latest Products</h1>
       <Row>
-        {products.map((product)=>(
-            <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
-                <Product product={product}/>
-            </Col>
+        {products.map((product) => (
+          <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
+            <Product product={product} />
+          </Col>
         ))}
       </Row>
     </>
